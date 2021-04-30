a9e743c43d95909919803c67e982a2548f71a8c2
gbrs <61385511+gbrs@users.noreply.github.com>
2021-04-30 13:10:25 +0300

    comparison 'python style for' vs 'for in range'

---
+++ for - range vs python style.py
@@ -0,0 +1,30 @@
+'''
+вау. Пайтон стайл таки всего раза в 1,5-2, но быстрее привычного range()
+'''
+
+
+import timeit
+
+NOT_REPITED_CODE = '''
+from random import randint
+N = 10000
+lst = [randint(0, N / 1000) for i in range(N)]
+# print(lst)
+sm = 0
+'''
+
+TESTED_CODE = ''' 
+for i in range(N):
+    sm += lst[i]
+
+# for number in lst:
+#     sm += number
+'''
+
+print(sum(timeit.repeat(stmt=TESTED_CODE, setup=NOT_REPITED_CODE, number=100))/5)
+
+
+'''
+python style: 0.113
+range: 0.194
+'''

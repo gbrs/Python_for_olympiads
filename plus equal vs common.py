e6bf4e9fe4b54b76df5515da267dd8f2e8916ed6
gbrs <61385511+gbrs@users.noreply.github.com>
2021-04-30 12:29:31 +0300

    сравнение a += 1 и a = a + 1

---
+++ plus equal vs common.py
@@ -0,0 +1,23 @@
+'''
+моно...уально
+'''
+
+
+import timeit
+
+NOT_REPITED_CODE = '''
+sm = 0
+'''
+
+TESTED_CODE = '''
+for i in range(10000):
+    sm = sm + 1000
+'''
+
+print(sum(timeit.repeat(stmt=TESTED_CODE, setup=NOT_REPITED_CODE, number=1000))/5)
+
+
+'''
++=  : 1.335
+_+_=: 1.324
+'''
